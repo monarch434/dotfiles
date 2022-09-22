1b3a5fe5531b4671978759977ac00fda06fbdbcb
amaquena <alvin1king@gmail.com>
2022-09-22 23:04:43 +0200

    removed iterm2

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 create_symlinks() {
     ln -shf ~/.dotfiles/.config/bat/ ~/.config/
     ln -shf ~/.dotfiles/.config/nvim/ ~/.config/
-    ln -shf ~/.dotfiles/.config/iterm2/ ~/.config/
-    ln -shf ~/.dotfiles/.config/simple-update-notifier/ ~/.config/
     ln -shf ~/.dotfiles/.config/spaceship-prompt/ ~/.config/
 } 
 
