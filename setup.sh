d198ea5aefeda6da9cb23d0fb4746c18f79fd216
Amaquena <alvin1king@gmail.com>
2024-08-14 12:28:44 +0200

    Added tmux-sessionizer and gitmux

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ create_symlinks() {
     ln -s ${PWD}/.config/bat/ ~/.config
     ln -s ${PWD}/.config/nvim/ ~/.config
     ln -s ${PWD}/.config/tmux/ ~/.config
-    ln -s ${PWD}/home/wezterm.lua ~/.wezterm.lua
+    ln -s ${PWD}/.config/wezterm ~/.config
     ln -s ${PWD}/home/.zshrc ~/.zshrc
+    ln -s ${PWD}/home/.gitmux.conf ~/.gitmux.conf
 } 
 
 create_symlinks
